d51a95a6c315e713bccce9cc0577dfa12bd08acb
Cameronmbell99 <cameronbellbchs@gmail.com>
2020-03-02 18:32:21 -0700

    Added the base html, some light styling in the css, and started the JQuery instructions

---
+++ Assets/JavaScript/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    // 1. create a nested array of questions
+    //      * add a boolean to each question
+    var Questions = [
+        ["How many people are there in the world?", false],
+        ["Is Hugh Jackman in this photo?", false],
+        ["Is this Hanibal Buress?", false],
+        ["Is this Lady Gaga's real name?", false],
+        ["Did Mark Hamill Play the Joker?", false],
+        ["Did Steven Spielburg get payed for his work on 'Schindler's List'?", false]
+    ];
+
+    // 2. create buttons for each question 
+    //      * set them true and false, set the boolean in each array to signify if they got the question right
+
+
+    // 3. set a timer to count down from [Undefined] minutes
+    //      * create an alert of the time to let the player know how long they have left
+
+    // 4. set an if statement, whether the player answers all the questions and submits them, or the time runs out
+    //      * if the time runs out take all the answers already selected, and set the unanswered questions to false
+    // 5. 
+    // 
+    // 
+
+
+
+
+
+});
