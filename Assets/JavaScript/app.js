0f4d3d1cf2049d7a10ea56685168d716f76850fd
Cameronmbell99 <cameronbellbchs@gmail.com>
2020-03-16 17:37:35 -0700

    fixed the timer running out, added images, and updated the questions

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -1,67 +1,71 @@
 window.onload = function() {
-    $("#Button1").on("click", function() { return isTrue(loop, "button1") });
-    $("#Button2").on("click", function() { return isTrue(loop, "button2") });
+    $("#Button1").on("click", function() { return isTrue("button1") });
+    $("#Button2").on("click", function() { return isTrue("button2") });
 };
 
 var Questions = [
-    ["How many people are there in the world?", true],
-    ["Is Hugh Jackman in this photo?", true],
+    ["Joker is the highest grossing R rated movie of all time?", true],
+    ["A human head was found in the infamous scene in 'The Godfather'?", false],
     ["Is this Hanibal Buress?", false],
-    ["Is this Lady Gaga's real name?", false],
+    ["In the matrix does Neo take the red pill?", true],
     ["Did Mark Hamill Play the Joker?", true],
     ["Did Steven Spielburg get payed for his work on 'Schindler's List'?", false]
 ];
 
-var images = [];
+var images = ["assets/Images/joker.jpg", "assets/Images/GodFather.jpg", "assets/Images/hannibalBuress.jpg", "assets/Images/Matrix.jpeg", "assets/Images/animatedJoker.jpg", "assets/Images/schindlersList.jpg"];
 
 // Variables
 var clockRunning = false;
-var time = 60;
+var time = 10;
 var wins = 0;
 var loses = 0;
-var loop = 0;
+var currentQuestion = 0;
 
-function isTrue(i, button) {
-    if (Questions[i[1]] === true && button === "button1") {
+function isTrue(button, i = currentQuestion) {
+    console.log(currentQuestion);
+    currentQuestion++;
+    if (Questions[i][1] === true && button === "button1") {
         alert("That is correct, on to the next question!");
         wins++
         reset();
         start();
-        i++;
-        display(Questions, images, i, 1);
-    } else if (Questions[i[1]] === false && button === "button2") {
+        display();
+    } else if (Questions[i][1] === false && button === "button2") {
         alert("That is correct, on to the next question!");
         wins++
         reset();
         start();
-        i++;
-        display(Questions, images, i, 1);
-    } else {
+        display();
+    } else if (Questions[i][1] === false && button === "button1" || Questions[i][1] === true && button === "button2") {
         alert("I'm sorry that's not right, better luck next time!");
         loses++
         reset();
         start();
-        i++;
-        display(Questions, images, i, 1);
+        display();
+        console.log(currentQuestion);
+    } else if (time === 0) {
+        alert("I'm sorry but you didn't answer in time!");
+        loses++
+        reset();
+        start();
+        display();
+        console.log(currentQuestion);
     }
 }
 
 
-function display(qArray, Iarray, i, p) {
-    for (loop = 0; loop < Questions.length; loop++) {
-        if (loop < Questions.length) {
-            $("#Questions").html(qArray[i[p]]);
-            $("#Images").html(Iarray[i]);
-        } else {
-            alert("Congrats you finished the quiz!");
-            alert("Wins: " + wins + "Loses: " + loses);
-        }
+function display(qArray = Questions, Iarray = images, i = currentQuestion) {
+    if (currentQuestion + 1 == qArray.length) {
+        $("#Questions").text(qArray[i][0]);
+        $("#Images").html("<img src = '" + Iarray[i] + "'>");
+    } else if (currentQuestion < qArray.length) {
+        $("#Questions").text(qArray[i][0]);
+        $("#Images").html("<img src = '" + Iarray[i] + "'>");
+    } else {
+        stop();
+        alert("Congrats you finished the quiz!");
+        alert("Wins: " + wins + " Loses: " + loses);
     }
-
-
-
-
-
 }
 
 // 3. set a timer to count down from [Undefined] minutes
@@ -69,34 +73,45 @@ function display(qArray, Iarray, i, p) {
 
 function start() {
 
-    //  TODO: Use setInterval to start the count here and set the clock to running.
     if (!clockRunning) {
         intervalId = setInterval(count, 1000);
         clockRunning = true;
     }
     if (time === 0) {
+        console.log("it reset");
         reset();
     }
     console.log("it's started");
 }
 
+function stop() {
+
+    clearInterval(intervalId);
+    clockRunning = false;
+}
+
 function reset() {
 
-    time = 60;
+    time = 30;
 
-    $("#Timer").html("01:00");
+    $("#Timer").html("00:30");
 
 }
 
 function count() {
 
-    //  TODO: increment time by 1, remember we cant use "this" here.
-    time--;
-    //  TODO: Get the current time, pass that into the timeConverter function,
-    //        and save the result in a variable.
-    var currentTime = timeConverter(time);
-    //  TODO: Use the variable you just created to show the converted time in the "display" div.
-    $("#Timer").text(currentTime);
+    if (time === 0) {
+        isTrue();
+    } else {
+
+        time--;
+
+        var currentTime = timeConverter(time);
+        console.log(time);
+
+        $("#Timer").text(currentTime);
+    }
+
 }
 
 function timeConverter(t) {
@@ -122,8 +137,8 @@ $(document).ready(function() {
 
     // 1. create a nested array of questions
     //      * add a boolean to each question
-
-
+    console.log("document ready ran");
+    display();
     start();
 
     // 4. set an if statement, whether the player answers all the questions and submits them, or the time runs out
